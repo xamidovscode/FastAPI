07a3544a5d9ebb7069bbcce21bcfd9dc45483079
xamidovscode <xamidovcoder@gmail.com>
2024-11-18 18:59:46 +0500

    user create, list

--- routers/user.py
+++ routers/user.py
@@ -1,24 +1,37 @@
-from fastapi import APIRouter, HTTPException  # Модуль для создания роутеров
-from models.user import User  # Импорт модели пользователя
+from fastapi import APIRouter, HTTPException
+
+from models.user import User
+from schemas.user import UserCreate, UsersList
 
 # Создаем роутер
 router = APIRouter()
 
-# Эндпоинт для создания пользователя
-@router.post("/users/")
-async def create_user(username: str, email: str, password: str):
-    hashed_password = f"hashed-{password}"  # Здесь должна быть логика хэширования
+
+@router.post("/user/create/")
+async def create_user(user_data: UserCreate):
+    # Проверяем, существует ли пользователь с таким email
+    existing_user = await User.filter(email=user_data.email).first()
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Пользователь с таким email уже существует",
+        )
+
+    # Хэшируем пароль (пример, лучше использовать библиотеку bcrypt)
+    hashed_password = f"hashed-{user_data.password}"
+
+    # Создаем пользователя
     user = await User.create(
-        username=username,
-        email=email,
+        username=user_data.username,
+        email=user_data.email,
         hashed_password=hashed_password,
     )
-    return {"id": user.id, "username": user.username, "email": user.email}
+    return user
 
-# Эндпоинт для получения пользователя по ID
-@router.get("/users/{user_id}")
-async def get_user(user_id: int):
-    user = await User.filter(id=user_id).first()  # Поиск пользователя по ID
-    if not user:
-        raise HTTPException(status_code=404, detail="Пользователь не найден")
-    return {"id": user.id, "username": user.username, "email": user.email}
+
+@router.get(
+    "/user/list",
+    response_model=list[UsersList]
+)
+async def get_all_users():
+    return await User.all()
---
+++ schemas/user.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, EmailStr, Field  # Импорт Pydantic моделей
+
+
+class UserCreate(BaseModel):
+    username: str   # Поле username с ограничением длины
+    email: EmailStr  # Email с автоматической валидацией
+    password: str
+
+    class Config:
+        orm_mode = True
+
+
+
+class UsersList(BaseModel):
+    username: str   # Поле username с ограничением длины
+    email: EmailStr  # Email с автоматической валидацией
+    hashed_password: str
+
+    class Config:
+        orm_mode = True
