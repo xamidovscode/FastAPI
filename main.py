79b83578a6ec4d7376570d2df760d2d591a8641a
xamidovscode <xamidovcoder@gmail.com>
2024-11-18 18:29:50 +0500

    structure

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise  # Подключение Tortoise ORM
+from routers import user  # Импорт роутеров
+
+# Создаем приложение FastAPI
+app = FastAPI()
+
+# Подключаем роутеры
+app.include_router(user.router)
+
+# Настройка Tortoise ORM
+register_tortoise(
+    app,
+    db_url="postgres://username:password@localhost:5432/mydatabase",  # URL для подключения к базе данных PostgreSQL
+    modules={"models": ["models.user"]},  # Путь к вашим моделям
+    generate_schemas=True,  # Автоматическое создание схем в базе данных
+    add_exception_handlers=True,  # Добавление обработчиков ошибок для базы данных
+)
+
+# Тестовый эндпоинт
+@app.get("/")
+async def root():
+    return {"message": "Добро пожаловать в проект FastAPI с Tortoise ORM"}
